1486a36ed5543814d25fd93fe540e196b2dd4a64
jhonnygit <jhonprograma@gmail.com>
2023-11-08 16:18:19 -0400

    login

--- services/authentication.service.js
+++ services/authentication.service.js
@@ -89,4 +89,20 @@ const userLogin=async (user)=>{
 
 };
 
-module.exports={userRegister, userLogin}
+const checkUserExist=async(query)=>{
+    let messages={
+        email:"user already existe",
+        username:"this username is taken",
+    };    
+    try{
+        let queryType=Object.keys(query)[0];
+        let userObject = await MongoDB.db
+            .collection(mongoConfig.collections.USERS)
+            .findOne(query);
+        return !userObject
+        ?{status:true, message:`This ${queryType} is not taken`}
+        :{status:false, message:messages[queryType]};        
+    } catch(error){}
+};
+
+module.exports={userRegister, userLogin,checkUserExist}
